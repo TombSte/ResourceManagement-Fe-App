ec2b650eae565c84aa8e02d81775f29786548b0f
Stefano Tomba <stefano.tomba@icloud.com>
2022-12-13 15:31:42 +0100

    Setup

---
+++ ResourceManagement-Fe-App/Authentication/AuthenticationHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
+
+namespace ResourceManagement_Fe_App.Authentication
+{ 
+	public interface IAuthenticationHelper
+	{
+		public Task<bool> IsLoggedAsync();
+		public UserData User { get; }
+        public Task<string> GetJWTAsync();
+    }
+
+    public interface IAuthenticationHelperWriter
+	{
+		public Task SetLocalAsync(string email, string jwt);
+	}
+
+    public class AuthenticationHelper : IAuthenticationHelper, IAuthenticationHelperWriter
+    {
+		private readonly IJSRuntime jsr;
+		public AuthenticationHelper(IJSRuntime jsr)
+		{
+			this.jsr = jsr;
+		}
+
+		public UserData User => throw new NotImplementedException();
+
+        public async Task<string> GetJWTAsync()
+        {
+            string jwt = await jsr.InvokeAsync<string>("localStorage.getItem", "userjwt").ConfigureAwait(false);
+            return jwt;
+
+		}
+
+        public async Task RefreshLocalEmailAsync()
+        {
+            string email = await jsr.InvokeAsync<string>("localStorage.getItem", "useremail").ConfigureAwait(false);
+        }
+
+        public async Task SetLocalAsync(string email, string jwt)
+        {
+            await jsr.InvokeVoidAsync("localStorage.setItem", "userjwt", $"{jwt}").ConfigureAwait(false);
+            await jsr.InvokeVoidAsync("localStorage.setItem", "useremail", $"{email}").ConfigureAwait(false);
+        }
+
+		public async Task<bool> IsLoggedAsync()
+		{
+            var jwt = await GetJWTAsync();
+            return jwt != null;
+		}
+	}
+}
---
+++ ResourceManagement-Fe-App/Authentication/AuthenticationModels.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceManagement_Fe_App.Authentication
+{
+    public class LoginResult
+    {
+        public string message { get; set; }
+        public string email { get; set; }
+        public string jwtBearer { get; set; }
+        public bool success { get; set; }
+    }
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Email address is not valid.")]
+        public string email { get; set; } // NOTE: email will be the username, too
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+    public class RegModel : LoginModel
+    {
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [DataType(DataType.Password)]
+        [Compare("password", ErrorMessage = "Password and confirm password do not match.")]
+        public string confirmpwd { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required.")]
+        [DataType(DataType.Text)]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required.")]
+        [DataType(DataType.Text)]
+        public string lastname { get; set; }
+    }
+
+    public class UserData
+    {
+
+    }
+}
