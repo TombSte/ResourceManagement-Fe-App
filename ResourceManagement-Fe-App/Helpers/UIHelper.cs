d5aa54990bfc925439f1f7b9bd5e47ebf1fbc786
Stefano Tomba <stefano.tomba@icloud.com>
2022-12-16 01:37:40 +0100

    Fix vari

---
+++ ResourceManagement-Fe-App/Helpers/UIHelper.cs
@@ -0,0 +1,27 @@
+﻿using ResourceManagement_Fe_App.Data;
+
+namespace ResourceManagement_Fe_App.Helpers
+{
+    public static class UIHelper
+    {
+        public static string GetColor(TransactionType type)
+        {
+            switch (type)
+            {
+                case TransactionType.Outgoing: return "volcano";
+                case TransactionType.Incoming: return "green";
+                default: return "geekblue";
+            }
+        }
+
+        public static string GetName(TransactionType type)
+        {
+            switch (type)
+            {
+                case TransactionType.Outgoing: return "Uscita";
+                case TransactionType.Incoming: return "Entrata";
+                default: return "Non Specificata";
+            }
+        }
+    }
+}
